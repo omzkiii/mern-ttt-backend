32eb6951bda78647995a30a321be8d941fc24ea9
omzkiii <geogeomarsantos@gmail.com>
2025-09-05 16:50:28 +0800

    build fix

--- dist/controllers/db.js
+++ dist/controllers/db.js
@@ -2,10 +2,7 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 const url = process.env.DB ?? "";
-const client = new MongoClient(url, {
-    tls: false,
-    ssl: false,
-});
+const client = new MongoClient(url);
 export async function connectDB() {
     await client.connect();
     const result = await client.db("admin").command({ ping: 1 });
